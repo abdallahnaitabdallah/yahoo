051f0ee5ad609737ab61bed1bbffdabf88d9aa58
abdallahnaitabdallah <123880917+abdallahnaitabdallah@users.noreply.github.com>
2023-01-31 11:12:23 +0100

    third commit

--- getUserdata.py
+++ getUserdata.py
@@ -6,28 +6,33 @@ from time import sleep
 username = 'abdallah_nait'
 pwd = 'Nait_abdallah592@'
 
-def get_user_info(driver):
+def get_user_info(browser):
+    
     user_info = {
-        "username": "",
-        "phone": ""
+        "firstname": str,
+        "lastname": str,
+        "phone": int
     }
-    driver.switch_to.window(driver.window_handles[0])
+    # return to tab with index 0 to genarate and get a user name 
+    browser.switch_to.window(driver.window_handles[0])
 
-    driver.find_element(By.ID,'genbtn').click()
-    user = driver.find_element(By.CLASS_NAME,'address').find_element(By.TAG_NAME,'h3').text
+    browser.find_element(By.ID,'genbtn').click()
+    user = browser.find_element(By.CLASS_NAME,'address').find_element(By.TAG_NAME,'h3').text
     user = user.split(' ')
     del user[1]
-    user_info["username"] = user
+    user_info["firstname"] = str(user[0])
+    user_info["lastname"] = str(user[1])
+
+    # return to tab with index 1 to select and get the phone number 
+    browser.switch_to.window(browser.window_handles[1])
     
-    # open new tab and point on it 
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    
-    selected_value = Select(driver.find_element(By.TAG_NAME,"select")).select_by_index(index)
-    options = [x for x in driver.find_element(By.TAG_NAME,"select").find_element(By.TAG_NAME,"option")]
+    selected_value = Select(browser.find_element(By.TAG_NAME,"select")).select_by_index(index)
+    user_info["phone"] = element.get_attribute('value')
     
     return user_info
 
-def get_validation_code(driver):
-    driver.switch_to.window(driver.window_handles[1])
-    drive.find_element(By.TAG_NAME,"form").submit()
+def get_validation_code(browser):
+    browser.switch_to.window(browser.window_handles[1])
+    browser.find_element(By.TAG_NAME,"form").submit()
+    
+    
